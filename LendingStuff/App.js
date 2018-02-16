e3c16676dd7e524a97c22eebc2518ce36d8dbb9b
rashidg <rashid.gaziyev@gmail.com>
2018-02-16 08:57:21 -0500

    Connect to my Firebase.

--- LendingStuff/App.js
+++ LendingStuff/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import * as firebase from 'firebase';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+      var database = firebase.database();
+
+      database.ref('db/').set({
+          items: [
+              "Macbook Charger",
+              "Iphone Charger",
+              "Android Charger"
+          ]
+      });
+
+      var itemList = [];
+      database.ref("db/").once("value", (res) => {
+          console.log(res.val().items);
+          itemList = res.val().items.map((item) => (<Text>{item}</Text>));
+      });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Hot Reload!!!</Text>
+          {itemList}
       </View>
     );
   }
@@ -22,3 +36,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDELTgbOTKEIK9ZKJ7qq4qoLKUJbUUJwTI",
+    authDomain: "lendingstuff-41688.firebaseapp.com",
+    databaseURL: "https://lendingstuff-41688.firebaseio.com",
+    projectId: "lendingstuff-41688",
+    storageBucket: "lendingstuff-41688.appspot.com",
+    messagingSenderId: "307235664110"
+};
+firebase.initializeApp(config);
