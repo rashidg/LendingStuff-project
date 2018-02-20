88687e30cf88c2a931a8bbe8b009b26e7146c14f
jason-utoronto <jason.cai@mail.utoronto.ca>
2018-02-20 14:48:06 -0500

    worked on SortBar

---
+++ lendingStuff/src/components/SortBar.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import { Text, View, Switch, Button, Alert} from 'react-native';
+
+
+
+class SortBar extends React.Component {
+
+	state = {
+
+		switchValue: true
+
+	};
+
+
+
+	_handleToggleSwitch = () => this.setState(state => ({
+
+		switchValue: !state.switchValue
+
+	}));
+
+
+
+	_handleButtonPress = () => {
+
+		Alert.alert(
+'Map View');
+
+	};
+
+	render() {
+
+		const style = {
+
+			flex: 1,
+
+			backgroundColor: '#fff',
+
+			alignItems: 'Center'
+
+		};
+
+
+		return (
+
+			<View style={style}>
+
+				<Text>Sort By</Text>
+
+				<Text>Distance</Text>
+
+
+				<Switch
+
+					onValueChange={this._handleToggleSwitch}
+
+					value={this.state.switchValue}
+
+				/>
+
+
+				<Text>Price</Text>
+
+
+				<Button
+
+					title="Map View"
+
+					onPress={this._handleButtonPress}
+
+				/>
+    
+			</View>
+
+		);
+
+	}
+
+}
