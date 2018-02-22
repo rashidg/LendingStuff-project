51c1f1e2d426ac588f64fc4a13ad4c99e8bd652f
chukl <alec.olson@mail.utoronto.ca>
2018-02-21 22:25:45 -0500

    App loads- want to implement image loading

--- LendingStuff/src/components/Post.js
+++ LendingStuff/src/components/Post.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Text, View, Button, TextInput } from 'react-native';
-
-import ItemList from './ItemList';
-import { fetchData } from '../actions';
+import { Text, View, Button, TextInput, Image } from 'react-native';
 
 class Post extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //Placeholder: srcless image should appear transparent
-      pic_src: "about:blank",
+      pic_src: "../stock_image.png",
 
       desc: "",
       dur: 0,
@@ -23,7 +19,10 @@ class Post extends React.Component {
   }
 
   replaceImage() {
-    //Placeholder: does nothing
+  }
+
+  updateIndex(index) {
+    this.state.categoryIdx = index;
   }
 
   render() {
@@ -35,63 +34,72 @@ class Post extends React.Component {
 
     return (
       <View style={style}>
-        //Placeholder: should get src of image from user, then
-        //replace picture.src
-        <img src=this.state.pic_src
-             height=20
-             width=20
-             onPress={() => this.replaceImage();}>
+        <Image source={{ uri: this.state.pic_src }}
+               style={width=50, height=50}/>
+        <Button title="Upload a new image"
+                onPress={() => {this.replaceImage();}}/>
         {
-          this.categories.map(
+          this.state.categories.map(
             (key, index) =>
-            ( if (index == categoryIdx) {
-                <Button title=key.toUpperCase()
-                        onPress={(index) => {this.state.categoryIdx = index;}}/>
+            {
+              if (index == this.state.categoryIdx) {
+                <Button title={String(key).toUpperCase()}
+                        onPress={(index) => updateIndex(index)} />
               }
               else {
-                <Button title=key.toLowerCase()
-                        onPress={(index) => {this.state.categoryIdx = index;}}/>
-              } )
+                <Button title={String(key).toLowerCase()}
+                        onPress={(index) => updateIndex(index)} />
+              }
+            }
+          )
         }
 
         <Text>Description:</Text>
         <TextInput
-          style={{height: 200}}
+          style={{height: 50, width: 200}}
           placeholder="Write an item description:"
           onChangeText={(text) => {this.state.desc = text;}} />
 
         <Text>Duration:</Text>
         <TextInput
-          style={{height: 200}}
+          style={{height: 50, width: 200}}
           placeholder="Enter the duration (in hours):"
           onChangeText={(text) => {
             if (parseInt(text) != NaN) {
               this.state.dur = parseInt(text);
             }
-            else {
-              placeholder="Enter the duration (in hours):";
-            }
-          }/>
+          }}/>
 
         <Text>Meeting_location:</Text>
         <TextInput
-          style={{height: 200}}
+          style={{height: 50, width: 200}}
           placeholder="Enter the meeting location:"
           onChangeText={(text) => {this.state.meetLoc = text;}} />
 
         <Text>Hourly_rate:</Text>
         <TextInput
-          style={{height: 200}}
+          style={{height: 50, width: 200}}
           placeholder="Enter the the hourly rate (in $):"
           onChangeText={(text) => {
             if (parseInt(text) != NaN) {
               this.state.rate = parseInt(text);
             }
-            else {
-              placeholder="Enter the the hourly rate (in $):";
-            }
-          }/>
+          }}/>
       </View>
     );
   }
 }
+
+export default connect(
+  (state) => {
+    return {
+      pic_src: state.pic_src,
+      desc: state.desc,
+      dur: state.dur,
+      meetLoc: state.meetLoc,
+      rate: state.rate,
+      categories: state.categories,
+      categoryIdx: state.categoryIdx
+    };
+  }
+)(Post);
