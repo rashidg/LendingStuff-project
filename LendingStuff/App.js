ac32fffddfbc2b769bf73595714033385fcfb30f
choesuh1 <donna.choe@mail.utoronto.ca>
2018-03-04 13:42:31 -0500

    sidebar implemented with link to few pages.

--- LendingStuff/App.js
+++ LendingStuff/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect, Provider } from 'react-redux';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, StatusBar } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Scene, Router, Stack, Drawer, Actions } from 'react-native-router-flux';
@@ -17,20 +17,20 @@ const reduxStore = createStore(indexReducer, applyMiddleware(thunk));
 export default () => (
   <Provider store={reduxStore}>
     <Router>
-      <Stack key="root" leftTitle="Left" onLeft={() => { Actions.pop(); }}>
-        <Drawer
-          hideNavBar
-          key="drawer"
-          contentComponent={SideBar}
-          drawerWidth={250}
-          drawerPosition="right"
-        >
-          <Scene key="home" component={Home} title="Home"/>
-          <Scene key="items" component={Items} title="Register"/>
-          <Scene key="post" component={Post} title="Post an Item"/>
-          <Scene key="search" component={Search} title="Search"/>
-        </Drawer>
-      </Stack>
+        <Stack key="root" back={true} >
+          <Drawer
+            hideNavBar
+            key="drawer"
+            contentComponent={SideBar}
+            drawerWidth={280}
+            drawerPosition="right"
+          >
+            <Scene key="home" back={false} component={Home} title="Home"/>
+            <Scene key="items" component={Items} title="Register"/>
+            <Scene key="post" component={Post} title="Post an Item"/>
+            <Scene key="search" component={Search} title="Search"/>
+          </Drawer>
+        </Stack>
     </Router>
   </Provider>
 );
