9ea1f2e4e6a93cb00a8dde29c2dded7fa5c76563
chukl <alec.olson@mail.utoronto.ca>
2018-03-04 15:58:32 -0500

    testing item detail page

--- LendingStuff/App.js
+++ LendingStuff/App.js
@@ -10,6 +10,7 @@ import Home from './src/components/Home';
 import Post from './src/components/Post';
 import indexReducer from './src/reducers/index';
 import Search from './src/components/Search'
+import ItemDetail from '.src/components/ItemDetail'
 
 const reduxStore = createStore(indexReducer, applyMiddleware(thunk));
 
@@ -21,6 +22,7 @@ export default () => (
         <Scene key="items" component={Items} title="Register"/>
         <Scene key="post" component={Post} title="Post an Item"/>
         <Scene key="search" component={Search} title="Search"/>
+        <Scene key="itemDetail" component={ItemDetail} title="Item Detail"/>
       </Stack>
     </Router>
   </Provider>
