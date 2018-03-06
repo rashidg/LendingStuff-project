74d29de72e0927fcae653783986bddf6cf950a5e
rashidg <rashid.gaziyev@gmail.com>
2018-03-05 23:11:52 -0500

    Reuse Categories in Search Page

--- LendingStuff/src/components/Search.js
+++ LendingStuff/src/components/Search.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import { View, Button, TextInput, Slider, Text, Alert } from 'react-native';
 import { Actions } from 'react-native-router-flux';
-import RadioForm from 'react-native-radio-form';
 
-//Ideally this list will be populated from the Database
-//Or we will make use of the components created by the others (ROW specifically)
-//Or we can hardcode the categories here since it is an MVP
-const categories = [
-	{
-		label: 'chargers'
-	},
-	{
-		label: 'adapters'
-	},
-	{
-		label: 'home products'
-	}
-];
+import { categories } from '../constants';
+import Categories from './Categories';
 
 export default class Search extends React.Component {
 
@@ -26,7 +13,8 @@ export default class Search extends React.Component {
 			text: '',
 			distance: 5,
 			duration: 12,
-			category: 'adapters',
+			categories: categories,
+			categoryIdx: 0,
 			price: 5
 		};
 	}
@@ -46,54 +34,48 @@ export default class Search extends React.Component {
 	}
 
 	render() {
-
     const style = {
       flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center'
+      backgroundColor: '#fff'
     };
     return (
 			<View style={style}>
-				<TextInput style={{height: 50, width: 300}}
-									 placeholder="Input your search"
-									 onChangeText={(text) => this.setState({text})} />
-				<Text>
-					Distance: {this.state.distance} KM
-				</Text>
-				<Slider style={{ width: 300 }}
-         				step={1}
-        				minimumValue={0}
-         				maximumValue={10}
-         				value={this.state.distance}
-         				onValueChange={(result) => this.setState({distance: result})} />
-				<Text>
-					Duration: {this.state.duration} hours
-				</Text>
-				<Slider style={{ width: 300 }}
-         				step={0.5}
-        				minimumValue={0}
-         				maximumValue={24}
-         				value={this.state.duration}
-         				onValueChange={(result) => this.setState({duration: result})} />
-				<Text>
-					Rate: {this.state.price} per hour
-				</Text>
-				<Slider style={{ width: 300 }}
-	        			step={5}
-        				minimumValue={0}
-          			maximumValue={500}
-	        			value={this.state.price}
-        				onValueChange={(result) => this.setState({price: result})} />
-				<RadioForm style={{ width: 300, height:70 }}
-									 dataSource={categories}
-									 itemShowKey="label"
-              		 itemRealKey="label"
-              		 circleSize={16}
-              		 initial={1}
-              		 formHorizontal={true}
-              		 labelHorizontal={true}
-              		 onPress={(item) => this.setState({category: item.label})} />
+				<View style={{alignItems: 'center', justifyContent: 'center'}}>
+					<TextInput style={{height: 50, width: 300}}
+										 placeholder="Input your search"
+										 onChangeText={(text) => this.setState({text})} />
+					<Text>
+						Distance: {this.state.distance} KM
+					</Text>
+					<Slider style={{ width: 300 }}
+									step={1}
+									minimumValue={0}
+									maximumValue={10}
+									value={this.state.distance}
+									onValueChange={(result) => this.setState({distance: result})} />
+					<Text>
+						Duration: {this.state.duration} hours
+					</Text>
+					<Slider style={{ width: 300 }}
+									step={0.5}
+									minimumValue={0}
+									maximumValue={24}
+									value={this.state.duration}
+									onValueChange={(result) => this.setState({duration: result})} />
+					<Text>
+						Rate: {this.state.price} per hour
+					</Text>
+					<Slider style={{ width: 300 }}
+									step={5}
+									minimumValue={0}
+									maximumValue={500}
+									value={this.state.price}
+									onValueChange={(result) => this.setState({price: result})} />
+				</View>
+
+				<Categories categories={this.state.categories}
+										categoryIdx={this.state.categoryIdx}
+										onPress={(index) => this.setState({categoryIdx: index})} />
 				<Button title="Submit search"
           			onPress={this.handleSearch.bind(this)} />
       </View>
