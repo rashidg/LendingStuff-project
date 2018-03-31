d8973d47778315f0c0cdac0f23cfbffa4954da14
Amr Sharaf <amrmagdy.sharaf@gmail.com>
2018-03-31 19:23:30 -0400

    Displays actual username now

--- LendingStuff/src/components/SideBar.js
+++ LendingStuff/src/components/SideBar.js
@@ -7,13 +7,22 @@ import {
   StyleSheet,
   ScrollView,
 } from 'react-native';
+
+import { connect } from 'react-redux'
+
 import {Actions} from "react-native-router-flux";
 
 const username = "lender";
 
-export default class SideBar extends React.Component {
+class SideBar extends React.Component {
 
   render() {
+    const { user } = this.props;
+    if(user != null){
+      displayName = user.displayName;
+    } else{
+      displayName = "Not logged in";
+    }
     return (
       <View style={styles.container}>
           <View style={styles.profile}>
@@ -21,7 +30,7 @@ export default class SideBar extends React.Component {
               <Image style={styles.image}
                      source={require('../image/man.png')} />
             </View>
-            <Text style={styles.username}>{username}</Text>
+            <Text style={styles.username}>{displayName}</Text>
           </View>
 
         <ScrollView>
@@ -127,3 +136,7 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
   }
 });
+const mapStateToProps = (state) => ({
+  user: state.auth.user
+});
+export default connect(mapStateToProps)(SideBar);
